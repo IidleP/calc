dd18e1d9b9ee20609d32d7a5081e596dd7449bbd
unknown <i.petrakov52@gmail.com>
2026-05-12 00:42:47 +0300

    Edit gitignore

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/repos/calc/calc/ANTLR/antlr4-cpp-runtime-4.13.2-source
+# Build directory: D:/repos/calc/calc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("runtime")
---
+++ runtime/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/repos/calc/calc/ANTLR/antlr4-cpp-runtime-4.13.2-source/runtime
+# Build directory: D:/repos/calc/calc/runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
